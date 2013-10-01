cbf1d2fe70ac834ac1747bd07622b2e586abe4e9
Himanshu Singh <91.himanshu@gmail.com>
2013-10-01 12:50:44 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Picturesque::Application.config.secret_key_base = '58c11fa39f28ec58899034f008025f56b2ca291e9a938288efa722a4cb310bc558553817f823179667f4ea84a0a88e609e82eadad336351c8af3e3c89e13cb7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Picturesque::Application.config.session_store :cookie_store, key: '_picturesque_session'
